4d31c447b56d1108efc6ad892a9506271a8cfe84
Gant <GantMan@gmail.com>
2013-09-30 11:16:44 -0500

    adding hello_world shell

---
+++ examples/hello_world/app/hello_world_controller.rb
@@ -0,0 +1,16 @@
+class HelloWorldController < UIViewController
+  def viewDidLoad
+    p "Hello World"
+
+#     view.backgroundColor = UIColor.whiteColor
+#     center_x = view.frame.size.width / 2
+#     center_y = view.frame.size.height / 2
+
+#     @label = UILabel.new
+#     @label.text = "Hello World"
+#     @label.sizeToFit
+#     @label.center = [center_x, center_y]
+
+#     view.addSubview(@label)
+  end
+end
